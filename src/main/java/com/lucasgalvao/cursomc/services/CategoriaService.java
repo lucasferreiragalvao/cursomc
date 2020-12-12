8197b7c715f4f9c454bb2c12f07b7cdc77db4f5e
Lucas Galvão <lucasferreiragalvaoti@gmail.com>
2020-12-12 11:24:55 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/lucasgalvao/cursomc/services/CategoriaService.java
+++ src/main/java/com/lucasgalvao/cursomc/services/CategoriaService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.lucasgalvao.cursomc.domain.Categoria;
 import com.lucasgalvao.cursomc.repositories.CategoriaRepository;
+import com.lucasgalvao.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);	
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
