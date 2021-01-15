d6b22d340d2032892efb49bf24810ca880dfbf37
Lucas Galvão <lucasferreiragalvaoti@gmail.com>
2021-01-14 22:02:16 -0300

    Pedido,EstadoPagamento e Pagamento

--- src/main/java/com/lucasgalvao/cursomc/CursomcApplication.java
+++ src/main/java/com/lucasgalvao/cursomc/CursomcApplication.java
@@ -1,6 +1,6 @@
 package com.lucasgalvao.cursomc;
 
-import java.util.ArrayList;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +13,20 @@ import com.lucasgalvao.cursomc.domain.Cidade;
 import com.lucasgalvao.cursomc.domain.Cliente;
 import com.lucasgalvao.cursomc.domain.Endereco;
 import com.lucasgalvao.cursomc.domain.Estado;
+import com.lucasgalvao.cursomc.domain.Pagamento;
+import com.lucasgalvao.cursomc.domain.PagamentoComBoleto;
+import com.lucasgalvao.cursomc.domain.PagamentoComCartao;
+import com.lucasgalvao.cursomc.domain.Pedido;
 import com.lucasgalvao.cursomc.domain.Produto;
+import com.lucasgalvao.cursomc.domain.enums.EstadoPagamento;
 import com.lucasgalvao.cursomc.domain.enums.TipoCliente;
 import com.lucasgalvao.cursomc.repositories.CategoriaRepository;
 import com.lucasgalvao.cursomc.repositories.CidadeRepository;
 import com.lucasgalvao.cursomc.repositories.ClienteRepository;
 import com.lucasgalvao.cursomc.repositories.EnderecoRepository;
 import com.lucasgalvao.cursomc.repositories.EstadoRepository;
+import com.lucasgalvao.cursomc.repositories.PagamentoRepository;
+import com.lucasgalvao.cursomc.repositories.PedidoRepository;
 import com.lucasgalvao.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -37,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private EnderecoRepository enderecoRepository;
 	@Autowired
 	private ClienteRepository clienteRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -86,6 +97,23 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
+	
 		
 	}
 	
--- src/main/java/com/lucasgalvao/cursomc/domain/Cliente.java
+++ src/main/java/com/lucasgalvao/cursomc/domain/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.lucasgalvao.cursomc.domain.enums.TipoCliente;
 
@@ -36,6 +37,9 @@ public class Cliente implements Serializable{
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {
 		
@@ -104,6 +108,15 @@ public class Cliente implements Serializable{
 
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
+	}
+	
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 
 	@Override
